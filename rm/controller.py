7455866f16d730244f52cef94792097d37ac4918
brumen <gorazd.brumen@gmail.com>
2019-07-20 18:25:07 -0400

    Transport changes.

--- rm/controller.py
+++ rm/controller.py
@@ -1,7 +1,16 @@
 # main controlling logic for the risk management
 
+import time
+import datetime
 import zmq
-from ao.mysql
+import sys
+sys.path.append('/home/brumen/work/rm/ao/')
+
+from typing import Dict, List
+
+from ao.mysql_connector_env import MysqlConnectorEnv
+from ao.air_option          import AirOptionMock
+
 
 class Controller:
     """ Main controlling logic.
@@ -9,30 +18,60 @@ class Controller:
     """
 
     def __init__(self
-                , port = 5556 ):
+                , mkt_date = None
+                , port     = 5556 ):
+
+        self._mkt_date = mkt_date if mkt_date else datetime.date.today()  # market date is today or provided date
 
         # zmq section of the controller
         self.port      = port
         self.__context = zmq.Context()
         self.__socket  = self.__context.socket(zmq.PAIR)
-        self.__socket.bind("tcp://*:{0}".format(self.port))
+        self.__socket.bind("tcp://*:{0}".format(self.port))  # server ip
 
         # signal handlers
         self.__is_revaluing_portfolio = False
+        self.__curr_delta = None
 
         self.__portfolio = []  # no portfolio
 
-    def start(self):
-        """ Start the controller.
+    @property
+    def curr_delta(self):
+        return self.__curr_delta
+
+    @curr_delta.setter
+    def curr_delta(self, new_delta):
+        self.__curr_delta = new_delta
+
+    @property
+    def curr_portfolio(self):
+        return self.__portfolio
+
+    @curr_portfolio.setter
+    def curr_portfolio(self, new_portfolio):
+        self.__portfolio = new_portfolio
+
+    def start(self
+             , sleep_time = .1 ):
+        """ Starts the controller.
 
         """
-        pass
 
-    def _read_portfolio(self):
+        while True:
+            msg_received = self.__socket.recv()
+            print(msg_received)
+            time.sleep(sleep_time)  # sleep .1 seconds
+
+    def _read_portfolio(self, db_host='localhost'):
+        """ Reads the entire portfolio from the database.
+
+        :returns:
         """
 
-        :return:
-        """
+        with MysqlConnectorEnv(host=db_host) as db_conn:
+            self.curr_portfolio = db_conn.cursor().execute('SELECT * FROM options_positions').fetchall()
+
+        return self.curr_portfolio
 
     def _new_market_event(self):
         """ What to do when a new market event occurs.
@@ -40,23 +79,96 @@ class Controller:
         :return:
         """
 
+        self.__is_revaluing_portfolio = True
+        self.curr_delta = Controller.__revalue_portfolio( self.curr_portfolio
+                                                        , self._mkt_date)
+        self.__is_revaluing_portfolio = False
 
-    def _new_position_event(self):
-        """ What to do when a new position comes in.
+    def _new_position_event(self, new_position_l : List) -> None:
+        """ Update the state What to do when a new position comes in.
 
-        :return:
+        :returns:
         """
 
-        pass
+        self.__portfolio.extend(new_position_l)
 
-    def _revalue_portfolio(self):
+        self.curr_delta = Controller.__merge_deltas( self.curr_delta
+                                                   , Controller.__revalue_portfolio( new_position_l
+                                                                                   , self._mkt_date)
+                                                   )
+
+    def _revalue_current_portfolio(self):
         """ Revalue the entire portfolio.
 
         :return:
         """
 
         self.__is_revaluing_portfolio = True
-
-        # TODO: SOMETHING
-
+        self.curr_delta = Controller.__revalue_portfolio(self.curr_portfolio, self._mkt_date)
         self.__is_revaluing_portfolio = False
+
+    @staticmethod
+    def __revalue_portfolio(portfolio, mkt_date):
+        """ Revalues the portfolio given.
+
+        :param portfolio:
+        :param mkt_date:
+        :return:
+        """
+
+        portfolio_delta = {}
+        for _, orig, dest\
+             , option_start_date\
+             , option_end_date\
+             , option_ret_start_date\
+             , option_ret_end_date\
+             , outbound_date_start\
+             , outbound_date_end\
+             , inbound_date_start\
+             , inbound_date_end\
+             , K\
+             , carrier\
+             , adults\
+             , cabinclass in portfolio:
+
+            ao_pv01  = AirOptionMock( mkt_date
+                                    , orig
+                                    , dest
+                                    , option_start_date = option_start_date
+                                    , option_end_date   = option_end_date
+                                    , option_ret_start_date = option_ret_start_date
+                                    , option_ret_end_date   = option_ret_end_date
+                                    , outbound_date_start   = outbound_date_start
+                                    , outbound_date_end     = outbound_date_end
+                                    , inbound_date_start    = inbound_date_start
+                                    , inbound_date_end      = inbound_date_end
+                                    , K                     = K
+                                    , carrier               = carrier
+                                    , adults                = adults
+                                    , cabinclass            = cabinclass ).PV01()
+
+            portfolio_delta = Controller.__merge_deltas(portfolio_delta, ao_pv01)
+
+        return portfolio_delta
+
+    @staticmethod
+    def __merge_deltas(delta_1 : Dict, delta_2 : Dict) -> Dict:
+        """ Merge the two deltas.
+
+        :param delta_1: delta dictionary, {'UA71': 1.,...}
+        :param delta_2: delta dictionary, {'UA71': 2, 'UA72': 1.,...}
+        :returns: resulting delta dictionary {'UA71': 3, 'UA72': 1.,...}
+        """
+
+        result_delta = {}
+
+        for delta_1_flight_nb, delta_1_flight_value in delta_1.items():
+            if delta_1_flight_nb in delta_2.keys():
+                result_delta[delta_1_flight_nb] = delta_1_flight_value + delta_2[delta_1_flight_nb]
+            else:
+                result_delta[delta_1_flight_nb] = delta_1_flight_value
+
+        for delta_2_flight_nb in set(delta_2.keys()).difference(set(delta_1.keys())):
+            result_delta[delta_2_flight_nb] = delta_2[delta_2_flight_nb]
+
+        return result_delta
--- rm/report_new_position.py
+++ rm/report_new_position.py
@@ -3,6 +3,7 @@
 # reports new position when it is inserted into a database
 #
 
+import sys
 import zmq
 
 
@@ -10,18 +11,14 @@ def send_message( position
                 , port = 5556 ) -> None:
     """ Sends the position to the controller.
 
-    :param position:
+    :param position: position to message to the controller listening on port.
     :param port: port on which to send
     """
 
-    context = zmq.Context()
-    socket  = context.socket(zmq.PAIR)
-    socket.bind("tcp://*:{0}".format(port))
-
+    socket  = zmq.Context().socket(zmq.PAIR)
+    socket.connect('tcp://localhost:{0}'.format(port))
     socket.send_json(position)
 
 
-# action to execute
-argv = 1
-argc = 2
-send_message(argv)
+# action to execute as a script
+send_message(str(sys.argv[1]))
