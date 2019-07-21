c64ba98295b498e4a6563ef2e3453650ecbfb472
brumen <gorazd.brumen@gmail.com>
2019-07-21 07:58:07 -0400

    Initial dict.

---
+++ rm/delta_dict.py
@@ -0,0 +1,36 @@
+#
+# Delta dictionary
+
+
+class DeltaDict(dict):
+
+    def __neg__(self, *args, **kwargs):
+        """ Reverses the sign of every delta.
+
+        :param delta: delta dictionary, {'UA71': 1.,...}
+        :returns: resulting delta dictionary {'UA71': -1,...}
+        """
+
+        return {delta_flight_nb: - delta_flight_value
+                for delta_flight_nb, delta_flight_value in self.items() }
+
+    def __add__(self, other):
+        """ Merge the two delta dicts.
+
+        :param other: other delta dictionary, {'UA71': 1.,...}
+        :param delta_2: delta dictionary, {'UA71': 2, 'UA72': 1.,...}
+        :returns: resulting delta dictionary {'UA71': 3, 'UA72': 1.,...}
+        """
+
+        result_delta = {}
+
+        for delta_1_flight_nb, delta_1_flight_value in self.items():
+            if delta_1_flight_nb in other.keys():
+                result_delta[delta_1_flight_nb] = delta_1_flight_value + other[delta_1_flight_nb]
+            else:
+                result_delta[delta_1_flight_nb] = delta_1_flight_value
+
+        for delta_2_flight_nb in set(other.keys()).difference(set(self.keys())):
+            result_delta[delta_2_flight_nb] = other[delta_2_flight_nb]
+
+        return result_delta
