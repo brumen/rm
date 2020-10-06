afb34dd644c1415ac37133c923aecf40ae01d4d3
brumen <gorazd.brumen@gmail.com>
2020-10-06 09:57:23 -0400

    refactoring producers.

--- market_ticker.py
+++ market_ticker.py
@@ -2,54 +2,15 @@
 
 import logging
 
-from time  import sleep
-from kafka import KafkaProducer
-from threading import Thread
+from rm.producer_base import ProducerBase
+
 
 logging.basicConfig(filename='/tmp/controller.log')
 logger = logging.getLogger(__name__)
 logger.setLevel('INFO')
 
 
-class MarketUpdater:
-
-    def __init__( self
-                , server_name : str = 'localhost'
-                , port        : int = 9092
-                , topic       : str = 'quickstart-events' ):
-
-        self._topic    = topic
-        self._producer = KafkaProducer(bootstrap_servers='{0}:{1}'.format(server_name, str(port)))
-
-    def _message(self):
-        raise NotImplementedError('Implement _message.')
-
-    def _run_fct(self, sleep_delay : float = 0.1):
-        """ Market producer function that is ran as a thread.
-
-        :returns:
-        """
-
-        while True:
-            message = self._message()
-            logger.debug('Sending new market: {0}'.format(message))
-            self._producer.send(self._topic, value=message)
-            sleep(sleep_delay)
-
-    def start(self, idle_delay : float = 0.1) -> Thread:
-        """ Run the controller.
-
-        :param idle_delay: delay of the IDLE state of the controller.
-        :returns: run the controller.
-        """
-
-        run_thread = Thread(target = self._run_fct, kwargs={'sleep_delay': idle_delay} )
-        run_thread.start()
-
-        return run_thread
-
-
-class MarketUpdaterJoke(MarketUpdater):
+class MarketUpdaterJoke(ProducerBase):
 
     def _message(self):
         return b'MARKET_EVENT_1'
--- positions_updater.py
+++ positions_updater.py
@@ -6,53 +6,14 @@ from time      import sleep
 from kafka     import KafkaProducer
 from threading import Thread
 
+from rm.producer_base import ProducerBase
+
 logging.basicConfig(filename='/tmp/controller.log')
 logger = logging.getLogger(__name__)
 logger.setLevel('INFO')
 
 
-class PositionUpdater:
-    """ Position updater process.
-    """
-
-    def __init__( self
-                , server_name : str = 'localhost'
-                , port : int        = 9092
-                , topic : str       = 'quickstart-events' ):
-
-        self._producer = KafkaProducer(bootstrap_servers='{0}:{1}'.format(server_name, str(port)))
-        self._topic    = topic
-
-    def _message(self):
-        raise NotImplementedError('Implement the _message method.')
-
-    def _run_fct(self, sleep_delay : float = 0.1):
-        """ Producer
-
-        :param sleep_delay: sleep delay for the producer
-        :return:
-        """
-
-        while True:
-            message = self._message()
-            logger.debug('Sending message: {0}'.format(message))
-            self._producer.send(self._topic, value=message)
-            sleep(sleep_delay)
-
-    def start(self, idle_delay : float = 0.1) -> Thread:
-        """ Run the controller.
-
-        :param idle_delay: delay of the IDLE state of the controller.
-        :returns: run the controller.
-        """
-
-        run_thread = Thread(target=self._run_fct, kwargs={'sleep_delay': idle_delay} )
-        run_thread.start()
-
-        return run_thread
-
-
-class PositionUpdaterJoke(PositionUpdater):
+class PositionUpdaterJoke(ProducerBase):
 
     def _message(self):
         return b'POSITION_1'
