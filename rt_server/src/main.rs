26c1333e823b8f269f9f5e631dc07d287f78aa73
brumen <gorazd.brumen@gmail.com>
2026-01-23 10:02:36 -0500

    fixing imports and main, not lib anymore

--- rt_server/src/main.rs
+++ rt_server/src/main.rs
@@ -6,33 +6,32 @@ use rdkafka::producer::FutureProducer;
 use std::sync::Arc;
 use tracing::{info, Level};
 
-mod all_markets;
-mod market;
-mod portfolio;
-mod portfolio_sender;
-mod pricer;
-mod publish;
-mod ref_deref;
-mod streaming;
-mod trade;
+pub mod market;
+pub mod portfolio;
+pub mod portfolio_sender;
+pub mod pricer;
+// pub mod process_trade;
+pub mod publish;
+pub mod ref_deref;
+pub mod streaming;
+pub mod trade;
+// pub mod trader;
 
-pub(crate) mod markets;
-
-// actor framework new
+pub(crate) mod all_markets;
 pub(crate) mod engine_actor;
 pub(crate) mod engine_letf;
+pub(crate) mod markets;
 pub(crate) mod mkt_handler_actor;
 pub(crate) mod processor_bulk;
 pub(crate) mod processor_curr;
 pub(crate) mod processor_middle;
 pub(crate) mod processor_msg;
 pub(crate) mod processor_new;
-// pub(crate) mod processor_setup;
-pub(crate) mod processor_setup_actor;
 pub(crate) mod trade_sender;
 pub(crate) mod trades;
+// pub(crate) mod processor_setup;  // TODO: include this after fixing the axum crate.
+pub(crate) mod processor_setup_actor;
 pub(crate) mod utils;
-// pub(crate) mod yf;
 
 use crate::engine_letf::start2;
 // use crate::markets::ao_market;
