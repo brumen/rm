79c27b4a55ddf55490e3b316cf3d98671c79aafc
brumen <gorazd.brumen@gmail.com>
2026-02-21 05:16:22 -0500

    removing Serialize, Deserialize on TradeRep structure. not needed.

--- rt_server/src/trade.rs
+++ rt_server/src/trade.rs
@@ -30,7 +30,7 @@ pub enum TradeError {
 
 /// Internal representations of trades.
 /// String is the trade id, TR is the trade representation.
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Clone)]
 pub struct TradeRep<TR>(pub DashMap<String, TR>);
 
 impl<TR> PartialEq for TradeRep<TR> {
